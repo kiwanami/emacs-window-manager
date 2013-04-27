2ad803e74410b71e989b4a3c521b9d327e7ad2f4
SAKURAI Masashi <m.sakurai@kiwanami.net>
2013-04-27 16:20:13 +0900

    Added package required info. (thx @syohex)

--- e2wm.el
+++ e2wm.el
@@ -1,10 +1,11 @@
 ;;; e2wm.el --- simple window manager for emacs
 
-;; Copyright (C) 2010, 2011  SAKURAI Masashi
+;; Copyright (C) 2010, 2011, 2013  SAKURAI Masashi
 
 ;; Author: SAKURAI Masashi <m.sakurai atmark kiwanami.net>
 ;; Version: 1.2
 ;; Keywords: tools, window manager
+;; Package-Requires: ((window-layout "1.2"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
